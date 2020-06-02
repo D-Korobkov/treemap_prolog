e4b8be56958d27b3a33cb057b41d2b8bce7b5e8f
D-Korobkov <dima.korobkovv@gmail.com>
2020-06-02 18:23:13 +0300

    fix rotates

--- tree-map.pl
+++ tree-map.pl
@@ -6,40 +6,58 @@ height(nil, tree(_, (_, _, H), _), Ans) :- Ans is H + 1, !.
 height(tree(_, (_, _, H1), _), tree(_, (_, _, H2), _), Ans) :- H1 > H2, Ans is H1 + 1, !.
 height(tree(_, (_, _, H1), _), tree(_, (_, _, H2), _), Ans) :- Ans is H2 + 1.
 
-%% balance (tree, balanced_tree)
-
-%% don't make any rotates
-balance(tree(nil, Data, nil), tree(nil, Data, nil)) :- !.
-balance(tree(nil, (K0, V0, H0), tree(L2, (K2, V2, H2), R2)), New_tree) :-
-	H0 < 2,
-	New_tree = tree(nil, (K0, V0, H0), tree(L2, (K2, V2, H2), R2)), !.
-balance(tree(tree(L1, (K1, V1, H1), R1), (K0, V0, H0), nil), New_tree) :-
-	H0 < 2,
-	New_tree = tree(tree(L1, (K1, V1, H1), R1), (K0, V0, H0), nil), !.
-balance(tree(tree(L1, (K1, V1, H1), R1), (K0, V0, H0), tree(L2, (K2, V2, H2), R2)), New_tree) :- 
-	abs(H2 - H1) < 2,
-	New_tree = tree(tree(L1, (K1, V1, H1), R1), (K0, V0, H0), tree(L2, (K2, V2, H2), R2)), !.
+%% count diff between heights of right subtree and left subtree (diff = height(right_child) - height(left_child))
+%% diff(right_child, left_child, diff)
+diff(nil, nil, 0).
+diff(tree(_, (_, _, H1), _), nil, Diff) :- Diff is H1 + 1, !.
+diff(nil, tree(_, (_, _, H2), _), Diff) :- Diff is -H2 - 1, !.
+diff(tree(_, (_, _, H1), _), tree(_, (_, _, H2), _), Diff) :- Diff is H1 - H2.
 
 %% left rotate
-balance(tree(nil, (K0, V0, H0), tree(L2, (K2, V2, H2), R2)), New_tree) :-
-	height(nil, L2, New_h0),
-	height(tree(This_l, (K0, V0, New_h0), This_r), R2, New_h2), 
-	New_tree = tree(tree(nil, (K0, V0, New_h0), L2), (K2, V2, New_h2), R2), !.
-balance(tree(tree(L1, (K1, V1, H1), R1), (K0, V0, H0), tree(L2, (K2, V2, H2), R2)), New_tree) :-
-	H2 > H1,
-	height(tree(L1, (K1, V1, H1), R1), L2, New_h0),
-	height(tree(This_l, (K0, V0, New_h0), This_r), R2, New_h2),
-	New_tree = tree(tree(tree(L1, (K1, V1, H1), R1), (K0, V0, New_h0), L2), (K2, V2, New_h2), R2), !.
+rotate_left(tree(L0, (K0, V0, H0), tree(L2, (K2, V2, H2), R2)), New_tree) :-
+	height(L0, L2, New_h0),
+	height(tree(_, (_, _, New_h0), _), R2, New_h2),
+	New_tree = tree(tree(L0, (K0, V0, New_h0), L2), (K2, V2, New_h2), R2).
 
 %% right rotate
-balance(tree(tree(L1, (K1, V1, H1), R1), (K0, V0, H0), nil), New_tree) :-
-	height(R1, nil, New_h0),
-	height(L1, tree(This_l, (K0, V0, New_h0), This_r), New_h1),
-	New_tree = tree(L1, (K1, V1, New_h1), tree(R1, (K0, V0, New_h0), nil)), !.
-balance(tree(tree(L1, (K1, V1, H1), R1), (K0, V0, H0), tree(L2, (K2, V2, H2), R2)), New_tree) :-
-	height(R1, tree(L2, (K2, V2, H2), R2), New_h0),
-	height(L1, tree(This_l, (K0, V0, New_h0), This_r), New_h1),
-	New_tree = tree(L1, (K1, V1, New_h1), tree(R1, (K0, V0, New_h0), tree(L2, (K2, V2, H2), R2))).
+rotate_right(tree(tree(L1, (K1, V1, H1), R1), (K0, V0, H0), R0), New_tree) :-
+	height(R1, R0, New_h0),
+	height(L1, tree(_, (_, _, New_h0), _), New_h1),
+	New_tree = tree(L1, (K1, V1, New_h1), tree(R1, (K0, V0, New_h0), R0)).
+
+%% balance (tree, balanced_tree)
+%% don't make any rotates
+balance(Diff, tree(L, Data, R), tree(L, Data, R)) :- abs(Diff) =< 1, !. 
+
+%% big left rotate
+balance(Diff, tree(L0, Data0, tree(L1, Data1, R1)), New_tree) :-
+	Diff =:= 2,
+	diff(R1, L1, Diff1),
+	Diff1 < 0,
+	rotate_right(tree(L1, Data1, R1), New_r),
+	rotate_left(tree(L0, Data0, New_r), New_tree), !.
+
+%% small left rotate
+balance(Diff, tree(L0, Data0, R0), New_tree) :-
+	Diff =:= 2,
+	rotate_left(tree(L0, Data0, R0), New_tree), !.
+
+%% big right rotate
+balance(_, tree(tree(L1, Data1, R1), Data0, R0), New_tree) :-
+	diff(R1, L1, Diff1),
+	Diff1 > 0,
+	rotate_left(tree(L1, Data1, R1), New_l),
+	rotate_right(tree(New_l, Data0, R0), New_tree), !.
+
+%% small right rotate
+balance(_, tree(L0, Data0, R0), New_tree) :-
+	rotate_right(tree(L0, Data0, R0), New_tree).
+
+%% procedure that balance tree (subtree)
+balance_tree(tree(L, (K, V, H), R), New_tree) :-
+	height(L, R, H),
+	diff(R, L, Diff),
+	balance(Diff, tree(L, (K, V, H), R), New_tree).
 
 %% insert(key, value, balanced_tree, new_balanced_tree)
 insert(Key, Value, nil, tree(nil, (Key, Value, 0), nil)) :- !. % create new node
@@ -47,16 +65,14 @@ insert(Key, Value, tree(L, (Key, _, Height), R), tree(L, (Key, Value, Height), R
 insert(Key, Value, tree(L, (K, V, H), R), New_tree) :- % if key is less than key in current node then go to the left subtree
 	Key < K,
 	insert(Key, Value, L, New_l),
-	height(New_l, R, New_h),
-	balance(tree(New_l, (K, V, New_h), R), New_tree), !.
+	balance_tree(tree(New_l, (K, V, New_h), R), New_tree), !.
 insert(Key, Value, tree(L, (K, V, H), R), New_tree) :- % else if key is greater than key in current node then go to the right subtree
 	insert(Key, Value, R, New_r),
-	height(New_r, L, New_h),
-	balance(tree(L, (K, V, New_h), New_r), New_tree).
+	balance_tree(tree(L, (K, V, New_h), New_r), New_tree).
 
 %% find_max(tree, min_key, value) -> search max key in tree
 find_max(tree(_, (K, V, _), nil), K, V). % if right subtree doesn't exist return <key, value>
-find_max(tree(L, (K, _, _), R), Min_k, V) :- find_max(R, Min_k, V). % else go to the right subtree
+find_max(tree(L, (K, _, _), R), Max_k, V) :- find_max(R, Max_k, V). % else go to the right subtree
 
 %% delete(key, tree, new_tree) -> delete (key, value) from tree
 delete(Key, nil, nil) :- !. % node<key, value> doesn't exist, delete nothing
@@ -65,20 +81,17 @@ delete(Key, tree(nil, (Key, _, _), nil), nil) :- !.
 delete(Key, tree(nil, (Key, _, _), R), R) :- !. 
 delete(Key, tree(L, (Key, _, _), nil), L) :- !.
 delete(Key, tree(L, (Key, _, _), R), New_tree) :- 
-	find_max(L, Min_k, V),
-	delete(Min_k, L, New_l),
-	height(New_l, R, New_h),
-	balance(tree(New_l, (Min_k, V, New_h), R), New_tree), !.
+	find_max(L, Max_k, V),
+	delete(Max_k, L, New_l),
+	balance_tree(tree(New_l, (Max_k, V, New_h), R), New_tree), !.
 %% search node<key, value>
 delete(Key, tree(L, (K, V, _), R), New_tree) :- % if key is less than key in current node then go to the left subtree
 	Key < K,
 	delete(Key, L, New_l),
-	height(New_l, R, New_h),
-	balance(tree(New_l, (K, V, New_h), R), New_tree), !.
+	balance_tree(tree(New_l, (K, V, New_h), R), New_tree), !.
 delete(Key, tree(L, (K, V, _), R), New_tree) :- % else if key is greater than key in current node then go to the right subtree
 	delete(Key, R, New_r),
-	height(New_r, L, New_h),
-	balance(tree(L, (K, V, New_h), New_r), New_tree).
+	balance_tree(tree(L, (K, V, New_h), New_r), New_tree).
 
 %% find(key, value, balanced_tree, result)
 find(K, V, tree(L, (K, V, _), R), found) :- !. % node exists
